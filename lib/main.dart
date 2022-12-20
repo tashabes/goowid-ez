78f7b722d145d22ce7a81fc9565217825bba25d1
Natasha Abel <Natasha.Abel@etztec.com>
2022-12-20 13:17:41 +0000

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,11 @@ import 'src/presentation/widgets/screens/Auth/signin_screen.dart';
 import 'src/presentation/widgets/screens/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
-Future <void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(MyApp());
 }
 
