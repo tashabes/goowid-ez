c138b627236d53eb61c7c6ed492f65fb29ca6f1b
Natasha Abel <107184300+tashabes@users.noreply.github.com>
2023-01-09 13:42:34 +0000

    Added appbar and french translation

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_fr.dart
@@ -0,0 +1,33 @@
+import 'app_localizations.dart';
+
+/// The translations for French (`fr`).
+class AppLocalizationsFr extends AppLocalizations {
+  AppLocalizationsFr([String locale = 'fr']) : super(locale);
+
+  @override
+  String get hello => 'Bonjour';
+
+  @override
+  String get yourDataYourChoice => 'Vos données. Votre choix.';
+
+  @override
+  String get signIn => 'S\'identifier';
+
+  @override
+  String get register => 'Enregistrer';
+
+  @override
+  String get enterYourEmail => 'Entrer votre Email';
+
+  @override
+  String get enterYourPassword => 'Tapez votre mot de passe';
+
+  @override
+  String get getInTouch => 'Entrer en contact';
+
+  @override
+  String get writeYourMessageHere => 'Ecrivez votre message ici...';
+
+  @override
+  String get send => 'Envoyer';
+}
---
+++ lib/classes/language.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class Language {
+  final int id;
+  final String flag;
+  final String name;
+  final String languageCode;
+
+  Language(this.id, this.flag, this.name, this.languageCode);
+
+  static List<Language> languageList() {
+    return <Language>[
+      Language(1, "🇬🇧", "English", "en"),
+      Language(2, "󠁧󠁢🇫🇷", "French", "fr"),
+    ];
+  }
+}
