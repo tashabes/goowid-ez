715cedffb67403dab81bf4fccaa64f00f910f821
Natasha Abel <107184300+tashabes@users.noreply.github.com>
2023-01-12 12:12:09 +0000

    Added bottom nav bar (hidden until functional)

---
+++ lib/src/presentation/widgets/models/rive_assets.dart
@@ -0,0 +1,35 @@
+import 'package:rive/rive.dart';
+
+class RiveAsset {
+  final String artboard, stateMachineName, title, src;
+  late SMIBool? input;
+
+  RiveAsset(this.src,
+      {required this.artboard,
+      required this.stateMachineName,
+      required this.title,
+      this.input});
+
+  set setInput(SMIBool status) {
+    input = status;
+  }
+}
+
+List<RiveAsset> bottomNavs = [
+  RiveAsset("lib/src/assets/RiveAssets/icons.riv",
+      artboard: "CHAT", stateMachineName: "CHAT_Interactivity", title: "Chat"),
+  RiveAsset("lib/src/assets/RiveAssets/icons.riv",
+      artboard: "SEARCH",
+      stateMachineName: "SEARCH_Interactivity",
+      title: "Search"),
+  RiveAsset("lib/src/assets/RiveAssets/icons.riv",
+      artboard: "TIMER",
+      stateMachineName: "TIMER_Interactivity",
+      title: "Timer"),
+  RiveAsset("lib/src/assets/RiveAssets/icons.riv",
+      artboard: "BELL", stateMachineName: "BELL_Interactivity", title: "Bell"),
+  RiveAsset("lib/src/assets/RiveAssets/icons.riv",
+      artboard: "USER",
+      stateMachineName: "USER_Interactivity",
+      title: "Profile"),
+];
