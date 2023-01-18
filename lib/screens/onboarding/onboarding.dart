215a6e5e01c7fed89eb6f44aee85a056c16b6b32
Natasha Abel <107184300+tashabes@users.noreply.github.com>
2023-01-18 12:48:19 +0000

    Added in Rive files

---
+++ lib/screens/onboarding/onboarding.dart
@@ -0,0 +1,136 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'package:rive/rive.dart';
+
+import 'components/animated_btn.dart';
+
+import 'components/sign_in_dialog.dart';
+
+class OnbodingScreen extends StatefulWidget {
+  const OnbodingScreen({super.key});
+
+  @override
+  State<OnbodingScreen> createState() => _OnbodingScreenState();
+}
+
+class _OnbodingScreenState extends State<OnbodingScreen> {
+  late RiveAnimationController _btnAnimationController;
+
+  bool isShowSignInDialog = false;
+
+  @override
+  void initState() {
+    _btnAnimationController = OneShotAnimation(
+      "active",
+      autoplay: false,
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            width: MediaQuery.of(context).size.width * 1.7,
+            left: 100,
+            bottom: 100,
+            child: Image.asset(
+              "assets/Backgrounds/Spline.png",
+            ),
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+              child: const SizedBox(),
+            ),
+          ),
+          const RiveAnimation.asset(
+            "assets/RiveAssets/shapes.riv",
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: const SizedBox(),
+            ),
+          ),
+          AnimatedPositioned(
+            top: isShowSignInDialog ? -50 : 0,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            duration: const Duration(milliseconds: 260),
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Spacer(),
+                    SizedBox(
+                      width: 260,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 180,
+                            child: Image.asset('assets/images/Goowid.png'),
+                          ),
+                          const Text(
+                            "Your data. Your choice.",
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: "Poppins",
+                              height: 1.2,
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+                          const Text(
+                            "",
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(flex: 2),
+                    AnimatedBtn(
+                      btnAnimationController: _btnAnimationController,
+                      press: () {
+                        _btnAnimationController.isActive = true;
+
+                        Future.delayed(
+                          const Duration(milliseconds: 800),
+                          () {
+                            setState(() {
+                              isShowSignInDialog = true;
+                            });
+
+                            showCustomDialog(
+                              context,
+                              onValue: (_) {
+                                setState(() {
+                                  isShowSignInDialog = false;
+                                });
+                              },
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                          "We strongly believe that your personal data is your business. When and where you share it is up to you. Through trusted guardianship, we empower you."),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
