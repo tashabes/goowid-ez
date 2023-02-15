5845c45b3f0d0a44d41927c7ec44c2ebbe9347cd
Natasha Abel <107184300+tashabes@users.noreply.github.com>
2023-02-15 16:14:04 +0000

    username

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -7,3 +7,6 @@ const addPassword = '/add_account';
 const otpVerify = '/otp_verify';
 const confirmPassword = '/confirm_password';
 const otpVerifyPhone = '/OTP_verify_phone';
+
+//bottom nav routes
+const btmNavProfile = "bottomNavItems[4]";
